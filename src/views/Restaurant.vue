17c4d52c588d78cb45bcd1fe8d834b6c3e0b4948
Nina19980108 <ninagirl8787@gmail.com>
2021-08-02 15:28:04 +0800

    feat: add restaurant detail component

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -2,6 +2,7 @@
   <div class="container py-5">
     <h1>餐廳描述頁</h1>
     <!-- 餐廳資訊頁 RestaurantDetail -->
+    <RestaurantDetail :initial-restaurant="restaurant" />
     <hr />
     <!-- 餐廳評論 RestaurantComments -->
     <!-- 新增評論 CreateComment -->
@@ -9,6 +10,7 @@
 </template>
 
 <script>
+import RestaurantDetail from "./../components/RestaurantDetail.vue";
 const dummyData = {
   restaurant: {
     id: 1,
@@ -95,6 +97,9 @@ export default {
       };
       this.restaurantComments = dummyData.restaurant.Comments;
     },
+  },
+  components: {
+    RestaurantDetail,
   },
 };
 </script>
