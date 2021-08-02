e31b0e571957831a9930132946250d21f27458d9
Nina19980108 <ninagirl8787@gmail.com>
2021-08-02 11:38:20 +0800

    feat: add user card component

---
+++ src/components/UserCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="col-3">
+    <a href="#">
+      <img :src="user.image" width="140px" height="140px" />
+    </a>
+    <h2>{{ user.name }}</h2>
+    <span class="badge bg-secondary"> 追蹤人數：{{ user.followerCount }}</span>
+    <p class="mt-3">
+      <button type="button" class="btn btn-danger" v-if="user.isFollowed">
+        取消追蹤
+      </button>
+      <button type="button" class="btn btn-primary" v-if="!user.isFollowed">
+        追蹤
+      </button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    initialUser: {
+      type: [String, Object],
+      required: true,
+    },
+  },
+  data() {
+    return {
+      user: this.initialUser,
+    };
+  },
+};
+</script>
--- src/views/UsersTop.vue
+++ src/views/UsersTop.vue
@@ -2,14 +2,63 @@
   <div class="container py-5">
     <NavTabs />
     <h1 class="mt-5">美食達人</h1>
+    <hr />
+    <div class="row text-center">
+      <UserCard v-for="user in topUsers" :key="user.id" :initial-user="user" />
+    </div>
   </div>
 </template>
 
 <script>
 import NavTabs from "./../components/NavTabs.vue";
+import UserCard from "./../components/UserCard.vue";
+
+const dummyData = {
+  topUsers: [
+    {
+      id: 1,
+      name: "User1",
+      image:
+        "https://i.pinimg.com/564x/0a/c4/ce/0ac4ce6e9c68fd3e3438581f636cc147.jpg",
+      followerCount: 8,
+      isFollowed: false,
+    },
+    {
+      id: 2,
+      name: "User2",
+      image:
+        "https://i.pinimg.com/564x/ea/89/77/ea89773b3f3dc9643290348678aa9d2f.jpg",
+      followerCount: 6,
+      isFollowed: true,
+    },
+    {
+      id: 3,
+      name: "User3",
+      image:
+        "https://i.pinimg.com/564x/88/d2/36/88d2360cf99cad25400fa72cdedea82b.jpg",
+      followerCount: 2,
+      isFollowed: false,
+    },
+  ],
+};
+
 export default {
+  data() {
+    return {
+      topUsers: [],
+    };
+  },
+  created() {
+    this.fetchTopUsers();
+  },
+  methods: {
+    fetchTopUsers() {
+      this.topUsers = dummyData.topUsers;
+    },
+  },
   components: {
     NavTabs,
+    UserCard,
   },
 };
 </script>
