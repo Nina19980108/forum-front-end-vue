73d7b68a213db251b4018c92377262af6e7d55e1
Nina19980108 <ninagirl8787@gmail.com>
2021-08-02 11:45:39 +0800

    feat: add follow/unfollow function

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -6,10 +6,20 @@
     <h2>{{ user.name }}</h2>
     <span class="badge bg-secondary"> 追蹤人數：{{ user.followerCount }}</span>
     <p class="mt-3">
-      <button type="button" class="btn btn-danger" v-if="user.isFollowed">
+      <button
+        type="button"
+        class="btn btn-danger"
+        v-if="user.isFollowed"
+        @click.stop.prevent="unfollow"
+      >
         取消追蹤
       </button>
-      <button type="button" class="btn btn-primary" v-if="!user.isFollowed">
+      <button
+        type="button"
+        class="btn btn-primary"
+        v-if="!user.isFollowed"
+        @click.stop.prevent="follow"
+      >
         追蹤
       </button>
     </p>
@@ -28,6 +38,24 @@ export default {
     return {
       user: this.initialUser,
     };
+  },
+  methods: {
+    follow() {
+      const num = this.user.followerCount + 1;
+      this.user = {
+        ...this.user,
+        isFollowed: true,
+        followerCount: num,
+      };
+    },
+    unfollow() {
+      const num = this.user.followerCount - 1;
+      this.user = {
+        ...this.user,
+        isFollowed: false,
+        followerCount: num,
+      };
+    },
   },
 };
 </script>
