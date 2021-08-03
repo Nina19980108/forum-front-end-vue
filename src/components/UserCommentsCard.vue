423fa38396da5713003c9854796ae596c44b62da
Nina19980108 <ninagirl8787@gmail.com>
2021-08-03 11:46:05 +0800

    feat: add UserCommentsCard component

---
+++ src/components/UserCommentsCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <div class="card-header"><strong>38</strong> 已評論餐廳</div>
+    <div class="card-body">
+      <a href="/restaurants/4">
+        <img
+        src=https://loremflickr.com/320/240/restaurant,food/?random&#x3D;42.58020446504014
+        width="60" height="60" class="avatar" />
+      </a>
+      <a href="/restaurants/6">
+        <img
+        src=https://loremflickr.com/320/240/restaurant,food/?random&#x3D;12.190112895855831
+        width="60" height="60" class="avatar" />
+      </a>
+      <a href="/restaurants/7">
+        <img
+        src=https://loremflickr.com/320/240/restaurant,food/?random&#x3D;26.38207413310656
+        width="60" height="60" class="avatar" />
+      </a>
+    </div>
+  </div>
+</template>
--- src/views/User.vue
+++ src/views/User.vue
@@ -13,27 +13,7 @@
         </div>
         <div class="col-md-8">
           <!-- 已評論餐廳 -->
-          <div class="card">
-            <div class="card-header"><strong>38</strong> 已評論餐廳</div>
-            <div class="card-body">
-              <a href="/restaurants/4">
-                <img
-                src=https://loremflickr.com/320/240/restaurant,food/?random&#x3D;42.58020446504014
-                width="60" height="60" class="avatar" />
-              </a>
-              <a href="/restaurants/6">
-                <img
-                src=https://loremflickr.com/320/240/restaurant,food/?random&#x3D;12.190112895855831
-                width="60" height="60" class="avatar" />
-              </a>
-              <a href="/restaurants/7">
-                <img
-                src=https://loremflickr.com/320/240/restaurant,food/?random&#x3D;26.38207413310656
-                width="60" height="60" class="avatar" />
-              </a>
-            </div>
-          </div>
-          <!-- // -->
+          <UserCommentsCard />
           <br />
           <!-- 收藏的餐廳 -->
           <div class="card">
@@ -60,12 +40,14 @@
 import UserProfileCard from "./../components/UserProfileCard.vue";
 import UserFollowingsCard from "./../components/UserFollowingsCard.vue";
 import UserFollowersCard from "./../components/UserFollowersCard.vue";
+import UserCommentsCard from "./../components/UserCommentsCard.vue";
 
 export default {
   components: {
     UserProfileCard,
     UserFollowingsCard,
     UserFollowersCard,
+    UserCommentsCard,
   },
 };
 </script>
