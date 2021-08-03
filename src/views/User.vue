a2c1a4ca846cbbf5cfe1ea0633ef2ef5d40080df
Nina19980108 <ninagirl8787@gmail.com>
2021-08-03 11:39:51 +0800

    feat: add UserProfileCard component

--- src/views/User.vue
+++ src/views/User.vue
@@ -2,36 +2,7 @@
   <div class="album py-5 bg-light">
     <div class="container">
       <!--  user card -->
-      <div class="card mb-3">
-        <div class="row no-gutters">
-          <div class="col-md-4">
-            <img
-              src="https://i.imgur.com/pU2mGov.png"
-              width="300px"
-              height="300px"
-            />
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">root123</h5>
-              <p class="card-text">root@example.com</p>
-              <ul class="list-unstyled list-inline">
-                <li><strong>38</strong> 已評論餐廳</li>
-                <li><strong>5</strong> 收藏的餐廳</li>
-                <li><strong>1</strong> followings (追蹤者)</li>
-                <li><strong>1</strong> followers (追隨者)</li>
-              </ul>
-              <p>
-                <a href="/users/1/edit"
-                  ><button type="submit" class="btn btn-primary">
-                    edit
-                  </button></a
-                >
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <UserProfileCard />
       <!-- // -->
       <div class="row">
         <div class="col-md-4">
@@ -113,3 +84,12 @@
     </div>
   </div>
 </template>
+
+<script>
+import UserProfileCard from "./../components/UserProfileCard.vue";
+export default {
+  components: {
+    UserProfileCard,
+  },
+};
+</script>
