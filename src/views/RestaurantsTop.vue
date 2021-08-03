5955b6b8bdc43b300c3628db28b4dc1f28147f3f
Nina19980108 <ninagirl8787@gmail.com>
2021-08-03 10:43:08 +0800

    feat: add restaurantsTop component

--- src/views/RestaurantsTop.vue
+++ src/views/RestaurantsTop.vue
@@ -2,14 +2,19 @@
   <div class="container py-5">
     <NavTabs />
     <h1 class="mt-5">人氣餐廳</h1>
+    <hr />
+    <RestaurantsTop />
   </div>
 </template>
 
 <script>
 import NavTabs from "./../components/NavTabs";
+import RestaurantsTop from "./../components/RestaurantsTop";
+
 export default {
   components: {
     NavTabs,
+    RestaurantsTop,
   },
 };
 </script>
