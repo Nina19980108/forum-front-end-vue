5d7ec3e8fd482427ca3e371f5a4e99ad599dd994
Nina19980108 <ninagirl8787@gmail.com>
2021-08-04 11:42:31 +0800

    feat: add GET user/edit route

---
+++ src/views/UserEdit.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container py-5">
+    <form>
+      <div class="mb-3">
+        <label for="name" class="form-label">Name</label>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          class="form-control"
+          placeholder="Enter Name"
+          required
+        />
+      </div>
+
+      <div class="mb-3">
+        <label for="image" class="form-label">Image</label>
+        <input
+          id="image"
+          type="file"
+          name="image"
+          accept="image/*"
+          class="form-control"
+        />
+      </div>
+
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</template>
