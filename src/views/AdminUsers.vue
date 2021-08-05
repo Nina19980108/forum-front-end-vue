f881a7f4bc21e810efd14d441d5a7567873a7939
Nina19980108 <ninagirl8787@gmail.com>
2021-08-05 10:59:12 +0800

    feat: add admin/users route

---
+++ src/views/AdminUsers.vue
@@ -0,0 +1,20 @@
+  <template>
+  <div class="container py-5">
+    <!-- AdminNav Component -->
+    <AdminNav />
+    <!-- AdminUsersTable -->
+    <AdminUsersTable />
+  </div>
+</template>
+
+<script>
+import AdminNav from "./../components/AdminNav.vue";
+import AdminUsersTable from "./../components/AdminUsersTable.vue";
+
+export default {
+  components: {
+    AdminNav,
+    AdminUsersTable,
+  },
+};
+</script>
